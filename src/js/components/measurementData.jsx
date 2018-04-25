6f3240e931336a86fa3237af57b83ad69ccfb40a
sampel404 <przemek.samel@gmail.com>
2018-04-25 15:30:05 +0200

    [fix]

--- src/js/components/measurementData.jsx
+++ src/js/components/measurementData.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class MeasurementData extends React.Component{
     render(){
-        console.log(this.props.);
+        
         return <h1>measurement</h1>
     }
 }
