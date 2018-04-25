71eb08a420eb624ae9ed941a245b261a58484e4e
Patryk <patrykrudzinski@gmail.com>
2018-04-25 15:35:13 +0200

    commit

--- src/js/components/measurementData.jsx
+++ src/js/components/measurementData.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 class MeasurementData extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: this.props.data
+        }
+    }
+
     render(){
-        console.log(this.props.);
         return <h1>measurement</h1>
     }
 }
