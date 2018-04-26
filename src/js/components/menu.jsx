87d1216a1345cc3f20a0b06a2258692b45a89067
sampel404 <przemek.samel@gmail.com>
2018-04-26 13:58:45 +0200

    [submenu]

---
+++ src/js/components/menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    Link
+} from 'react-router-dom';
+// import submenu from './submenu.js';
+
+
+
+class Menu extends React.Component {
+    render() {
+        return (
+            <nav className="submenu-nav">
+                <ul className="submenu-nav-list">
+                    {this.props.menu.map((el,i) => {
+                        return (
+                            <li className="submenu-nav-list-el" key={i}>
+                                <Link to={el.urL}>
+                                    <i className={`icon-sl-outline ${el.icon} text-primary font-s36`}></i>
+                                    <div className="separator half"></div>{el.text}
+                                    </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export {Menu}
